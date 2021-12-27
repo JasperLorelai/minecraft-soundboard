99194f70628f68ae6760b2364a57201058120858
JasperLorelai <wolf.haci12@gmail.com>
2021-12-27 08:11:17 +0100

    Update to 1.18.1

--- Config.js
+++ Config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    version: "1.17.1",
-    versionCompared: "1.16.5",
-    soundboardVersion: "7.0.0"
+    version: "1.18.1",
+    versionCompared: "1.17.1",
+    soundboardVersion: "8.0.0",
+    soundboardVersionCompared: "7.0.0"
 }
