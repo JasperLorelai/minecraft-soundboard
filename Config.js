cfad8007ec1161e630a47963cc5ad6f9b60ba832
JasperLorelai <wolf.haci12@gmail.com>
2022-02-03 18:19:10 +0100

    Fix old material name error

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     version: "1.18.1",
     versionCompared: "1.17.1",
-    soundboardVersion: "8.0.0",
+    soundboardVersion: "8.0.1",
     soundboardVersionCompared: "7.0.0"
 }
