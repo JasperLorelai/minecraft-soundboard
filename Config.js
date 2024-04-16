d860454c70d288167f9f1522c8e739b94da793fd
JasperLorelai <wolf.haci12@gmail.com>
2024-04-16 21:21:55 +0200

    chore: Bump version and build

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 module.exports = {
     version: "1.20.4",
-    versionCompared: "1.20.1",
-    soundboardVersion: "12.0.0",
-    soundboardVersionCompared: "11.0.0"
+    versionCompared: "1.20.4",
+    soundboardVersion: "12.0.1",
+    soundboardVersionCompared: "12.0.0"
 }
