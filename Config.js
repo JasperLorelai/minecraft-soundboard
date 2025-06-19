a5a4fe4e3a05be8c3d1500e66a7505cc130ae767
JasperLorelai <wolf.haci12@gmail.com>
2025-06-19 05:54:30 +0200

    feat: Update to 1.21.6

--- Config.js
+++ Config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    version: "1.21.5",
-    versionCompared: "1.21.4",
-    soundboardVersion: "17.0.0",
-    soundboardVersionCompared: "16.0.0"
+    version: "1.21.6",
+    versionCompared: "1.21.5",
+    soundboardVersion: "18.0.0",
+    soundboardVersionCompared: "17.0.0"
 }
